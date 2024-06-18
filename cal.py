e154f2d3018d7b6bb9afb89c60ee5cc37e62f2fa
Maxym Matskiv <matskiv2005@gmail.com>
2024-06-18 13:04:57 +0300

    Added TODO, changed style for buttons

--- cal.py
+++ cal.py
@@ -9,15 +9,20 @@ root.geometry("400x300")
 
 entry=Entry(root)
 entry.grid(row=0, column=0, columnspan=4)
-
+#TODO: Fix edit input
 def click(btn):
     text = btn
     if text == "=":
         try:
-             # Replace "5!" with "factorial(5)"
-            expr = re.sub(r"(\d+)!", r"factorial(\1)", entry.get())
+            expr = entry.get()
+            # Replace "^" with "**"
+            expr = expr.replace("^", "**")
+            # Replace "5!" with "factorial(5)"
+            expr = re.sub(r"(\d+)!", r"factorial(\1)", expr)
             # Replace "(2+3)!" with "factorial(2+3)"
             expr = re.sub(r"\(([^)]+)\)!", r"factorial(\1)", expr)
+            #TODO:Fix factorial with power
+
             result = eval(expr, {'__builtins__': None, 'factorial': factorial, 'sin': sin, 'cos': cos, 'tan': tan, 'cot': cot, 'ln': ln, 'sqrt': sqrt})
             entry.delete(0, END)
             entry.insert(END, result)
@@ -32,8 +37,8 @@ def click(btn):
         entry.delete(0, END)
     elif text == "del":
         entry.delete(len(entry.get())-1, END)
-    elif text == "pi":
-        entry.insert(END, math.pi)
+    elif text == "pi": #TODO: Fix pi and e input
+        entry.insert(END, math.pi) 
     elif text == "e":
         entry.insert(END, math.e)
     else:
@@ -73,15 +78,15 @@ buttons = [
     '(', ')', 'pi', 'e',
     'ln', 'sqrt', '^',
     'C', '!', 'sin', 'cos', 'tan', 'cot', 'exit', 'del'
-]
+] #TODO: Add logo instead of text
 
 i = 0
 for btn in buttons:
     def command(btn=btn):
         click(btn)
-    Button(root, text=btn, width=10, relief='ridge', activebackground='orange', command=command).grid(row=i//4+1, column=i%4)
+    Button(root, text=btn, width=10,activebackground='orange', command=command).grid(row=i//4+1, column=i%4)
     i += 1
-
+#TODO: Upgrade style
 root.bind('<Key>', key_press)
 
 root.mainloop()
